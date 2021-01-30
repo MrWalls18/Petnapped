d40f1762ec072b4d8b154706b9e2604d52993941
MrWalls18 <77751885+MrWalls18@users.noreply.github.com>
2021-01-29 23:51:15 -0800

    Scene Setup

---
+++ Petnapped! The Game/Assets/Scripts/Interact.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    [SerializeField]private Camera playerCamera;
+    [SerializeField]private float interactDistance;
+    [SerializeField]private LayerMask layerMask;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit interactableObject;
+
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out interactableObject, interactDistance, layerMask))
+        {
+            Debug.Log("Press E to interact");
+
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                InteractWith(interactableObject);
+            }
+        }
+    }
+
+    void InteractWith(RaycastHit interactableObject)
+    {
+        switch (interactableObject.transform.tag)
+        {
+            case "Key":
+                Destroy(interactableObject.transform.gameObject);
+                break;
+            case "Door":
+                    Debug.LogWarning("Key is needed!");
+                break;
+            case "Tutorial Door":
+                Destroy(interactableObject.transform.gameObject);
+                break;
+            default:
+                Debug.LogError("Error: Incorrect tag: " + interactableObject.transform.tag);
+                break;
+        }
+    }
+}
