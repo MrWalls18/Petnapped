45bb339972f1196c33ec544d7f81cd85244de47f
MrWalls18 <77751885+MrWalls18@users.noreply.github.com>
2021-01-29 21:26:02 -0800

    Created First Person Character

---
+++ Petnapped! The Game/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Numerics;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField]private float mouseSensitivity = 100f;
+
+    [SerializeField]private Transform playerBody;
+
+    private float xRotation = 0f; 
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Gets mouse movement on X and Y axis
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+
+        xRotation -= mouseY; //decreases x rotation based on mouse y
+
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //locks rotation between up and down
+
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+
+
+        //Rotates player body around the Y axis (Vector3.up)
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Petnapped! The Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]private CharacterController controller;
+
+    [SerializeField]private float walkingSpeed; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * xInput + transform.forward * zInput;
+
+        controller.Move(move * walkingSpeed * Time.deltaTime);
+
+    }
+}
